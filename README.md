afd214ca0de2b569f1d643623133769965224196
tangyuxuan <623521553@qq.com>
2022-04-17 23:50:45 +0800

    add person info manage system code

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# test
+this is the person info manage system!
+
+1.make
+    make
+2.clean
+    make clean
+3.run
+    ./test
--- test.c
+++ test.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 
+struct person_info
+{
+    char name[20];
+    char sex[20];
+    char age[20];
+};
+
+void person_info_show(struct person_info p_i)
+{
+    printf("name: %s\n", p_i.name);
+    printf("sex: %s\n", p_i.sex);
+    printf("age: %s\n", p_i.age);
+    
+}
 int main(int argc, char * argv[])
 {
-    
-    printf("helloword!\n");
+    struct person_info p_i = {0};
+
+    printf("what is your name?\n");
+    scanf("%s", p_i.name);
+    printf("what is your sex?\n");
+    scanf("%s", p_i.sex);
+    printf("what is your age?\n");
+    scanf("%s", p_i.age);
+
+    person_info_show(p_i);
+
     return 0;
 }
